7b31760292519a9d9a1a0e0145fa6188b7820bf6
windofshadow <33241347+windofshadow@users.noreply.github.com>
2018-10-02 23:20:36 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 
-time ./word2vec -train text8-norm1-phrase1 -output vectors.bin -cbow 0 -size 200 -window 10 -negative 25 -hs 0 -sample 1e-5 -threads 8 -binary 1 -iter 5
+time ./phrase2vec -train text8-norm1-phrase1 -output vectors.bin -cbow 0 -size 200 -window 10 -negative 25 -hs 0 -sample 1e-5 -threads 8 -binary 1 -iter 5
 ./distance vectors-phrase.bin
